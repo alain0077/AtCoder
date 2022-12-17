518b59e377371130ceda11cb867d95206a718c6e
alain0077 <alain@ngc.is.ritsumei.ac.jp>
2022-12-17 23:17:18 +0900

    ABC_282

---
+++ ABC_282/C.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <stack>
+#include <iomanip>
+#include <cmath>
+
+#define dbg(x) cout << #x << " = " << x << endl
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define chmax(a,b) (a<b?(a=b,1):0)
+#define chmin(a,b) (a>b?(a=b,1):0)
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define elif else if
+
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+const int INF = 100100100;
+const double ESP = 1e-9;
+
+int main()
+{
+    int n;
+    string s;
+    cin >> n;
+    cin >> s;
+
+    bool flag = false;
+
+    rep(i, s.size()) {
+        if (!flag && s[i] == '"') flag = true;
+        else if (flag && s[i] == '"') flag = false;
+        else if (!flag && s[i] == ',') s[i] = '.';
+    }
+
+    cout << s << endl;
+}
